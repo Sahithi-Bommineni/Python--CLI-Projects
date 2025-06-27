bc9ab8ade8dc997503b7aa866dd74dd508aaf429
sahithi-bommineni <bommineni.s@northeastern.edu>
2025-06-26 23:08:38 -0500

    uodated

--- todolist.py
+++ todolist.py
@@ -1,17 +1,29 @@
 print("-----Todo List-----")
 tasks=[]
 while True:
-    option=input("What would you like to do?\nAdd, Show, Edit, Complete or Exit?").lower()
+    option=input("What would you like to do?\nAdd, Show, Edit, Complete or Exit? ").lower()
     match option:
         case 'add':
-            todo = input("Enter todo:")
+            todo = input("Enter todo: ")
             tasks.append(todo)
-            print(tasks)
+            #print(tasks)
         case 'show':
-            print(tasks)
+            for i,task in enumerate(tasks):
+                print(i+1,"",task.title())
+        case 'edit':
+            for i,task in enumerate(tasks):
+                print(i+1,"",task.title())
+            n=int(input("mention task number to edit: "))
+            n=n-1
+            newtask = input("Enter new task: ")
+            tasks[n]= newtask
         case 'complete':
-            print(tasks)
-            n=input("which task do you want to mark complete? (mention task number)")
-            
+            for i,task in enumerate(tasks):
+                print(i+1,"",task.title())
+            n=int(input("mention task number to edit: "))
+            n=n-1
+            tasks.pop(n)
         case 'exit':
             break
+        case unknown:
+            print("Hey! You entered invalid option")
