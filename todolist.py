0b1447cda92e4cfbd17592b75d0d0b092bab6c3b
sahithi-bommineni <bommineni.s@northeastern.edu>
2025-07-02 11:45:06 -0500

    adding file to store data of todolist

--- todolist.py
+++ todolist.py
@@ -1,21 +1,36 @@
 print("-----Todo List-----")
-tasks=[]
+#tasks=[]
 while True:
     option=input("What would you like to do?\nAdd, Show, Edit, Complete or Exit? ").lower()
     match option:
         case 'add':
-            todo = input("Enter todo: ")
+            todo = input("Enter todo: ") + "\n"
+            file = open('todolist.txt','r') #open or create file
+            tasks = file.readlines() #read the file
+            file.close()
             tasks.append(todo)
+            file = open('todolist.txt','w') 
+            file.writelines(tasks) #write into file
+            file.close()
             #print(tasks)
         case 'show':
+            file = open('todolist.txt','r')
+            tasks = file.readlines()
             for i,task in enumerate(tasks):
                 print(i+1,"",task.title())
+            file.close()
         case 'edit':
+            file = open('todolist.txt','r')
+            tasks = file.readlines()
+            file.close()
             for i,task in enumerate(tasks):
                 print(i+1,"",task.title())
             n=int(input("mention task number to edit: "))
             n=n-1
             newtask = input("Enter new task: ")
+            file = open('todolist.txt','w') 
+            file.writelines(tasks) #write into file
+            file.close()
             tasks[n]= newtask
         case 'complete':
             for i,task in enumerate(tasks):
