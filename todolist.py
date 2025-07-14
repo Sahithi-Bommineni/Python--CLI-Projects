4d1667101905418b7b8b734710afe954c5578fa3
sahithi-bommineni <bommineni.s@northeastern.edu>
2025-07-14 11:42:03 -0500

    with functions to optimize code

--- todolist.py
+++ todolist.py
@@ -1,3 +1,13 @@
+def read_file(filename,filearg='r'):
+    with open(filename+'.txt',filearg) as file:
+        tasks = file.readlines()
+    return tasks
+
+def write_file(filename,tasks,filearg='w'):
+    with open(filename+'.txt',filearg) as file:
+        file.writelines(tasks)
+
+
 print("-----Todo List-----")
 #tasks=[]
 while True:
@@ -5,47 +15,64 @@ while True:
     match option:
         case 'add':
             todo = input("Enter todo: ") + "\n"
-            with open('todolist.txt','r') as file: #open or create file
-                tasks = file.readlines() #read the file
+            tasks = read_file("todolist","r")
+            # with open('todolist.txt','r') as file: #open or create file
+            #     tasks = file.readlines() #read the file
             #file.close() --> when using with open, we dont need to close explicitly
             tasks.append(todo)
-            file = open('todolist.txt','w') 
-            file.writelines(tasks) #write into file
-            file.close()
+            write_file("todolist",tasks,"w")
+            # file = open('todolist.txt','w') 
+            # file.writelines(tasks) #write into file
+            # file.close()
             #print(tasks)
         case 'show':
-            with open('todolist.txt','r') as file:
-                tasks = file.readlines()
+            tasks = read_file("todolist","r")
+            # with open('todolist.txt','r') as file:
+            #      tasks = file.readlines()
+            #read_file('todolist','r')
             for i,task in enumerate(tasks):
                 print(i+1,"",task.title())
         case 'edit':
-            with open('todolist.txt','r') as file:
-                tasks = file.readlines()
-            for i,task in enumerate(tasks):
-                print(i+1,"",task.title())
-            n=int(input("mention task number to edit: "))
-            n=n-1
-            newtask = input("enter task: ")
-            newtasks = []
-            for task in tasks:
-                if task[n] in task:
-                    newtasks.append(task.replace(task,newtask))
+            try:
+                tasks = read_file("todolist","r")
+                # with open('todolist.txt','r') as file:
+                #     tasks = file.readlines()
+                for i,task in enumerate(tasks):
+                    print(i+1,"",task.title())
+                n=int(input("mention task number to edit: "))
+                n=n-1
+                if n<len(tasks):
+                    newtask = input("enter task: ")
+                    tasks[n]=newtask + "\n"
+                    write_file("todolist",tasks,"w")
                 else:
-                    newtasks.append(task[n])
-            with open('todolist.txt','w') as file:
-                file.writelines(newtasks) #write into file
+                    print("Invalid task input. Please try again.\n")
+                    continue
+            except ValueError:
+                print("Invalid input, please try again.")
+                continue
         case 'complete':
-            with open('todolist.txt','r') as file:
-                tasks = file.readlines()
-            for i,task in enumerate(tasks):
-                print(i+1,"",task.title())
-            n=int(input("mention task number to edit: "))
-            n=n-1
-            file = open('todolist.txt','w') 
-            file.writelines(tasks) #write into file
-            tasks.remove(n)
-            file.close()
+            # with open('todolist.txt','r') as file:
+            #     tasks = file.readlines()
+            # for i,task in enumerate(tasks):
+            #     print(i+1,"",task.title())
+            try:
+                n=int(input("mention task number to edit: "))
+                tasks = read_file("todolist","r")
+                # with open('todolist.txt','r') as file:
+                #     tasks = file.readlines()
+                index=n-1
+                if index<len(tasks):
+                    task_to_remove = tasks[index].strip("\n")
+                    tasks.pop(index)
+                    write_file("todolist",tasks,"w")
+                else:
+                    print("Invalid task. Please try again.\n")
+                    continue
+            except ValueError:
+                print("Invalid input, please try again.")
+                continue
         case 'exit':
             break
         case unknown:
-            print("Sorry! You entered invalid option. Please try again.")
+            print("Sorry! You entered invalid option. Please try again.\n")
